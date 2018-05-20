abbd3e28d20b6cc7a1d04c8be12d24cf92298373
KJ Paul <paul@kjpaul.com>
2018-05-19 20:59:17 -0700

    git fucked

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 /*
 Github loves to fuck with me!
+
+And the fucking continuues. Where is my gaddamn code!
 */
 class App extends Component {
   render() {
